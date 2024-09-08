823f6950d8da85efbbea30211777cb6d0e482d00
seinmin <minsein596@gmail.com>
2024-09-08 13:41:33 +0630

    module-3 01,02 activities

---
+++ books/Front_End/M03_JavaScript/JS/01-Conditional-Statements.js
@@ -0,0 +1,38 @@
+// TODO: Change the values and operators below to test your algorithm meets all conditions
+var x = 50;
+var expression1 = x < 25;
+var expression2 = x > 39;
+
+if(expression1 && expression2){
+    console.log("True,True");
+}
+else if(expression1){
+    console.log("True,False");
+}
+else if(expression2){
+    console.log("False,True");
+}else{
+    console.log("False");
+}
+
+// TODO: Write Your JavaScript Code Here
+// x=prompt("Write a number between 40 and 55");
+// if(x==50){
+//     console.log("True");
+// }
+// else if(x<25){
+//     console.log("False");
+// }
+// else{
+//     console.log("False")
+// }
+// switch(x){
+//     case x==50:
+//         console.log("false");
+//         brake;
+//     case x==30:
+//         console.log("False");
+//         brake;
+//     default:    
+//         console.log("True");
+// }
---
+++ books/Front_End/M03_JavaScript/JS/02-Iteration.js
@@ -0,0 +1,6 @@
+// TODO: Write Your JavaScript Code Here
+var student=["Mg Mg","Kyaw Kyaw","Htun Htun"];
+
+for(var i=0; i<student.length; i++){
+    console.log("Great to see you, CLASSMATE_NAME!  " + student[i]);
+}
